b635b45667463f1e00b572d79106d5c7024df916
Rajan Sharma <email.rajan001@gmail.com>
2025-07-01 23:02:01 +0300

    courseUpdate

---
+++ components/dashboard/UpdateCourse.tsx
@@ -0,0 +1,357 @@
+"use client";
+import React, { FormEvent, useEffect, useState } from "react";
+import {
+  CourseProps,
+  input,
+  lable,
+  languages,
+  levels,
+} from "./styles/inputField";
+import { useProtectedRoute } from "../../context/useProtected";
+import { useAuth } from "../../context/authContext";
+import { CiSettings } from "react-icons/ci";
+import FormButton from "../FormButton";
+
+interface Props {
+  courseId: string;
+  updateTable: () => void;
+}
+const UpdateCourse = ({ courseId, updateTable }: Props) => {
+  console.log("Id for fetch: ", courseId);
+
+  useProtectedRoute(["admin"]);
+  const { user } = useAuth();
+  const [course, setCourses] = useState<CourseProps | null>();
+  const [cat, setCat] = useState([]);
+
+  const [hasMsg, setHasMsg] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    slug: "",
+    description: "",
+    thumbnail: "",
+    categoryId: "",
+    isFree: false,
+    price: "",
+    discount: "",
+    level: "",
+    language: "",
+    isPublished: false,
+  });
+
+  const getCourse = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/course/get-course/${courseId}`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const result = await res.json();
+    console.log("final check", result.getCourseById?.title);
+    setFormData({
+      title: result.getCourseById?.title || "",
+      slug: result.getCourseById?.slug || "",
+      description: result.getCourseById?.description || "",
+      thumbnail: result.getCourseById?.thumbnail || "",
+      categoryId: result.getCourseById?.categoryId || "",
+      isFree: result.getCourseById?.isFree || false,
+      price: result.getCourseById?.price || "",
+      discount: result.getCourseById?.discount || "",
+      level: result.getCourseById?.level || "",
+      language: result.getCourseById?.language || "",
+      isPublished: result.getCourseById?.isPublished || false,
+    });
+  };
+
+  const getCat = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/category/get-category`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const result = await res.json();
+    setCat(result.fetchCategory);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement | any
+    >
+  ) => {
+    const { name, type, value, checked } = e.target;
+
+    setFormData((prev) => {
+      if (type === "checkbox") {
+        return { ...prev, [name]: checked }; // boolean
+      }
+      return { ...prev, [name]: value }; // string/number
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/course/update-course/${courseId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "PATCH",
+          credentials: "include",
+          body: JSON.stringify(formData),
+        }
+      );
+      const result = await res.json();
+      if (!res.ok) {
+        setHasMsg(true);
+        setMsg(result.msg);
+        setLoading(false);
+      } else {
+        setLoading(false);
+        setHasMsg(true);
+        setMsg(result.msg);
+        setFormData({
+          title: "",
+          slug: "",
+          description: "",
+          thumbnail: "",
+          categoryId: "",
+          isFree: false,
+          price: "",
+          discount: "",
+          level: "",
+          language: "",
+          isPublished: false,
+        });
+        updateTable();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  setTimeout(() => {
+    setHasMsg(false);
+  }, 3000);
+
+  useEffect(() => {
+    getCourse();
+    getCat();
+  }, []);
+
+  return (
+    <div>
+      <div className=" w-full p-6">
+        {hasMsg && (
+          <div className=" w-full flex flex-col justify-center items-center bg-white p-6 border-1 border-gray-300 mb-5">
+            <p className="text-[12px] font-semibold capitalize text-red-400">
+              {msg}
+            </p>
+          </div>
+        )}
+        {loading ? (
+          <div className="flex flex-col items-center justify-center">
+            <CiSettings className="animate-spin" />
+            loading
+          </div>
+        ) : (
+          <form
+            onSubmit={handleSubmit}
+            className="w-full flex flex-col gap-5 items-center justify-center"
+          >
+            <div className="flex w-full gap-3">
+              <div className="w-4/12">
+                <label className={`${lable}`} htmlFor="title">
+                  Title
+                </label>
+                <input
+                  className={`${input}`}
+                  value={formData.title}
+                  onChange={handleChange}
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Course title"
+                />
+              </div>
+              <div className="w-4/12">
+                <label className={`${lable}`} htmlFor="slug">
+                  Slug
+                </label>
+                <input
+                  className={`${input}`}
+                  value={formData.slug}
+                  onChange={handleChange}
+                  type="text"
+                  id="slug"
+                  name="slug"
+                  placeholder="course slug"
+                />
+              </div>
+              <div className="w-4/12">
+                <label className={`${lable}`} htmlFor="thumbnail">
+                  Thumbnail URL
+                </label>
+                <input
+                  className={`${input}`}
+                  value={formData.thumbnail}
+                  onChange={handleChange}
+                  type="text"
+                  id="thumbnail"
+                  name="thumbnail"
+                  placeholder="course thumbnail"
+                />
+              </div>
+            </div>
+
+            <div className="flex w-full gap-3">
+              <div className="w-4/12">
+                {" "}
+                <label className={`${lable}`} htmlFor="language">
+                  Language
+                </label>
+                <select
+                  name="language"
+                  id=""
+                  className={`${input}`}
+                  value={formData.language}
+                  onChange={handleChange}
+                >
+                  {languages.map((lang, index) => (
+                    <option key={index} value={lang} className={``}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="w-4/12">
+                {" "}
+                <label className={`${lable}`} htmlFor="categoryId">
+                  Category
+                </label>
+                <select
+                  name="categoryId"
+                  id=""
+                  className={`${input}`}
+                  value={formData.categoryId}
+                  onChange={handleChange}
+                >
+                  {cat.map((cat: any) => (
+                    <option key={cat._id} value={cat._id} className={``}>
+                      {cat.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="w-2/12 flex justify-center items-center ">
+                <label className={`${lable}`} htmlFor="isFree">
+                  Is Free
+                </label>
+                <input
+                  className={`${input}`}
+                  checked={formData.isFree}
+                  onChange={handleChange}
+                  type="checkbox"
+                  id="isFree"
+                  name="isFree"
+                />
+              </div>
+              <div className="w-2/12 flex justify-center items-center ">
+                <label className={`${lable}`} htmlFor="isPublished">
+                  Is Published
+                </label>
+                <input
+                  className={`${input}`}
+                  checked={formData.isPublished}
+                  onChange={handleChange}
+                  type="checkbox"
+                  id="isPublished"
+                  name="isPublished"
+                />
+              </div>
+            </div>
+
+            <div className="flex w-full gap-3">
+              <div className="w-4/12">
+                <label className={`${lable}`} htmlFor="price">
+                  Price
+                </label>
+                <input
+                  className={`${input}`}
+                  value={formData.price}
+                  onChange={handleChange}
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Course price"
+                />
+              </div>
+              <div className="w-4/12">
+                {" "}
+                <label className={`${lable}`} htmlFor="discount">
+                  Discount (%)
+                </label>
+                <input
+                  className={`${input}`}
+                  value={formData.discount}
+                  onChange={handleChange}
+                  type="number"
+                  id="discount"
+                  name="discount"
+                  placeholder="dicount price"
+                />
+              </div>
+              <div className="w-4/12">
+                <label className={`${lable}`} htmlFor="level">
+                  Level
+                </label>
+                <select
+                  name="level"
+                  id=""
+                  className={`${input}`}
+                  value={formData.level}
+                  onChange={handleChange}
+                >
+                  {levels.map((level, index) => (
+                    <option key={index} value={level} className={``}>
+                      {level}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="flex w-full gap-3">
+              <div className="w-10/12">
+                <label className={`${lable}`} htmlFor="description">
+                  Description
+                </label>
+                <textarea
+                  value={formData.description}
+                  onChange={handleChange}
+                  rows={4}
+                  id="description"
+                  name="description"
+                  className={`${input}`}
+                  placeholder="Course description"
+                ></textarea>
+              </div>
+
+              <div className="w-2/12 flex justify-center items-center">
+                <FormButton title="Submit" />
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UpdateCourse;
--- src/app/(dashboard)/admin/dashboard/courses/page.tsx
+++ src/app/(dashboard)/admin/dashboard/courses/page.tsx
@@ -1,15 +1,33 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useProtectedRoute } from "../../../../../../context/useProtected";
-import { CourseProps } from "../../../../../../components/dashboard/styles/inputField";
+import {
+  button,
+  CourseProps,
+} from "../../../../../../components/dashboard/styles/inputField";
 import { useAuth } from "../../../../../../context/authContext";
 import Image from "next/image";
+import { BiEdit } from "react-icons/bi";
+import { FiDelete } from "react-icons/fi";
+import { CiSettings } from "react-icons/ci";
+import UpdateCourse from "../../../../../../components/dashboard/UpdateCourse";
 
+interface CatProps {
+  title: string;
+  _id: string;
+}
 const AllCourses = () => {
   useProtectedRoute(["admin"]);
   const { user } = useAuth();
-  const [courses, setCourses] = useState<CourseProps[] | null>([]);
+  const [courses, setCourses] = useState<CourseProps[] | null>();
   const [cat, setCat] = useState([]);
+
+  const [hasMsg, setHasMsg] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [updatePop, setUpdatePop] = useState(false);
+  const [courseId, setCourseId] = useState("");
 
   const getCourse = async () => {
     const res = await fetch(
@@ -35,14 +53,61 @@ const AllCourses = () => {
     setCat(result.fetchCategory);
   };
 
+  const handleDelete = async (id: any) => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/course/delete-course/${id}`,
+        {
+          method: "DELETE",
+          credentials: "include",
+        }
+      );
+      const result = await res.json();
+      if (!res.ok) {
+        setLoading(false);
+        setHasMsg(true);
+        setMsg(result.msg);
+      } else {
+        setLoading(false);
+        setHasMsg(true);
+        setMsg("Course deleted successfully.");
+        getCourse();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  setTimeout(() => {
+    setHasMsg(false);
+  }, 3000);
+
+  const updateTable = () => {
+    getCourse();
+  };
+
   useEffect(() => {
     getCourse();
     getCat();
+    updateTable();
   }, []);
 
   return (
-    <div className="w-full">
+    <div className="w-full relative">
       <div className="w-full">
+        {hasMsg && (
+          <div className=" w-full flex flex-col justify-center items-center bg-white p-6 border-1 border-gray-300 mb-5">
+            <p className="text-[12px] font-semibold capitalize text-red-400">
+              {msg}
+            </p>
+          </div>
+        )}
+        {loading && (
+          <div className="flex flex-col items-center justify-center">
+            <CiSettings className="animate-spin" />
+            loading
+          </div>
+        )}
         <div className="min-w-fit border border-gray-300 rounded-md">
           <div className="flex bg-gray-100 font-semibold text-[12px] text-gray-700">
             <div className="w-40 h-8 flex items-center justify-center border-r border-gray-300">
@@ -78,7 +143,7 @@ const AllCourses = () => {
             </div>
 
             <div className="w-20 h-8 flex items-center justify-center">
-              Created By
+              Action
             </div>
           </div>
 
@@ -86,7 +151,7 @@ const AllCourses = () => {
             {courses?.map((course) => (
               <div
                 key={course._id}
-                className=" flex text-[12px] text-gray-800 border-t bg-white border-gray-200  "
+                className=" flex text-[12px] text-gray-800 border-t bg-white border-gray-200"
               >
                 <div className="w-40 h-16 flex items-center justify-center border-r border-gray-100 ">
                   {course.title}
@@ -103,8 +168,8 @@ const AllCourses = () => {
                   />
                 </div>
                 <div className="w-30 h-16 flex items-center justify-center border-r border-gray-100">
-                  {cat.find((c: any) => c._id === course.categoryId)?.title ||
-                    "unknown"}
+                  {cat.find((c: CatProps) => c._id === course.categoryId)
+                    ?.title || "unknown"}
                 </div>
                 <div className="w-20 h-16 flex items-center justify-center border-r border-gray-100">
                   {course.isFree ? "Free" : "Paid"}
@@ -125,14 +190,44 @@ const AllCourses = () => {
                   {course.isPublished ? "Published" : "Unpublished"}
                 </div>
 
-                <div className="w-20 h-16 flex items-center justify-center ">
-                  {course.createdBy === user._id && user.fullname}
+                <div className="w-20 h-16 flex items-center justify-center gap-2">
+                  <button
+                    onClick={() => {
+                      setUpdatePop(true);
+                      setCourseId(course._id as string);
+                    }}
+                    className={`${button}`}
+                  >
+                    <BiEdit />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(course._id)}
+                    className={`${button}`}
+                  >
+                    <FiDelete />
+                  </button>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
+
+      {/* update pop */}
+
+      {updatePop && (
+        <div className="w-full shadow-md shadow-gray-400  absolute inset-0 top-0 backdrop-blur-2xl p-6">
+          <div className="w-full flex justify-end">
+            <button
+              onClick={() => setUpdatePop(false)}
+              className={`${button} `}
+            >
+              close
+            </button>
+          </div>
+          <UpdateCourse courseId={courseId} updateTable={updateTable} />
+        </div>
+      )}
     </div>
   );
 };
