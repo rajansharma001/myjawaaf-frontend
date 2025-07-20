73656f018410225c1c157f7d36b2a87fc14e5a75
Rajan Sharma <email.rajan001@gmail.com>
2025-07-20 21:17:12 +0300

    fixes miner issues

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { PiBell } from "react-icons/pi";
 import { useAuth } from "../../context/authContext";
 import { UserProps } from "./styles/inputField";
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 const Header = () => {
   const { user, logoutUser } = useAuth();
